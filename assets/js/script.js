87d572c08bfa227aeae66965d584af6d9f18c98a
Nicolas Gautier <nicolas.gautier@lapiscine.pro>
2023-08-15 14:34:49 +0200

    Contatc form - layout ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,9 @@
 // SVG
 
 const svg = document.getElementsByClassName('svgObject');
-console.log(svg)
 for(i = 0; i<svg.length; i++) {
     svg[i].addEventListener('mouseover', function(event){
         const svgDoc = event.target;
-        console.log(svgDoc)
         svgDoc.setAttribute('fill', '#4EE1A0')
     })
 }
@@ -20,8 +18,3 @@ for(i = 0; i<svg.length; i++) {
 // END SVG
 
 
-// OPEN URL
-
-function openUrl(){
-    window.open('https://www.github.com')
-}

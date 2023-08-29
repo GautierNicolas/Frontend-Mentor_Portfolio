8dd7c80e42c865c33c27fed2be81a4a54556b16b
Nicolas Gautier <nicolas.gautier@lapiscine.pro>
2023-08-29 10:50:28 +0200

    Add ReadMe

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,37 @@
 // SVG
 
 const svg = document.getElementsByClassName('svgObject');
-for(i = 0; i<svg.length; i++) {
+for(i = 0; i < svg.length; i++) {
     svg[i].addEventListener('mouseover', function(event){
         const svgDoc = event.target;
-        svgDoc.setAttribute('fill', '#4EE1A0')
+        // svgDoc.setAttribute('fill', '#4EE1A0')
+        svgDoc.style.filter = 'invert(5%) sepia(70%) saturate(1000%) hue-rotate(80deg) brightness(95%) contrast(95%)'
     })
 }
-for(i = 0; i<svg.length; i++) {
+for(i = 0; i < svg.length; i++) {
     svg[i].addEventListener('mouseout', function(event){
         const svgDoc = event.target;
-        svgDoc.setAttribute('fill', '#FFF')
+        // svgDoc.setAttribute('fill', '#FFF')
+        svgDoc.style.filter = 'none'
+
     })
 }
 
 // END SVG
 
+function handleEmailChange(event) {
+    const input = event.target;
 
+    const email = input.value;
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(emailRegex.test(email) && email.length > 3) {
+        input.style.borderBottom = '2px solid #4EE1A0';
+    } else {
+        input.style.borderBottom = '2px solid red';
+    }
+}
+// Contact Form
+
+
+// END contact form
